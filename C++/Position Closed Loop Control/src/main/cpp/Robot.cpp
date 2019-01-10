ccf3158571b91513b120cfd8a6215a5edeb02662
unknown <ryanjharmon1@gmail.com>
2019-01-10 12:30:41 -0600

    Updated device ID and comments

--- C++/Position Closed Loop Control/src/main/cpp/Robot.cpp
+++ C++/Position Closed Loop Control/src/main/cpp/Robot.cpp
@@ -12,11 +12,20 @@
 
 class Robot : public frc::TimedRobot {
   // initialize motor, pid controller and encoder
-  rev::CANSparkMax m_motor{1, rev::CANSparkMax::MotorType::kBrushless};
+  static const int deviceID = 1;
+  rev::CANSparkMax m_motor{deviceID, rev::CANSparkMax::MotorType::kBrushless};
+
+  /**
+   * In order to use PID functionality for a controller, a CANPIDController object
+   * is constructed by calling the GetPIDController() method on an existing
+   * CANSparkMax object
+   */
   rev::CANPIDController m_pidController = m_motor.GetPIDController();
+
+  // Encoder object created to display position values
   rev::CANEncoder m_encoder = m_motor.GetEncoder();
 
-  // default PID coefficients
+  // PID coefficients
   double kP = 0.1, kI = 1e-4, kD = 1, kIz = 0, kFF = 0, kMaxOutput = 1, kMinOutput = -1;
 
  public:
@@ -29,7 +38,7 @@ class Robot : public frc::TimedRobot {
     m_pidController.SetFF(kFF);
     m_pidController.SetOutputRange(kMinOutput, kMaxOutput);
 
-    // display PID coefficients on the SmartDashboard
+    // display PID coefficients on SmartDashboard
     frc::SmartDashboard::PutNumber("P Gain", kP);
     frc::SmartDashboard::PutNumber("I Gain", kI);
     frc::SmartDashboard::PutNumber("D Gain", kD);
@@ -50,7 +59,7 @@ class Robot : public frc::TimedRobot {
     double min = frc::SmartDashboard::GetNumber("Min Output", 0);
     double rotations = frc::SmartDashboard::GetNumber("Set Rotations", 0);
 
-    // if PID coefficients have changed, writ new values to controller
+    // if PID coefficients on SmartDashboard have changed, write new values to controller
     if((p != kP)) { m_pidController.SetP(p); kP = p; }
     if((i != kI)) { m_pidController.SetI(i); kI = i; }
     if((d != kD)) { m_pidController.SetD(d); kD = d; }
@@ -61,11 +70,24 @@ class Robot : public frc::TimedRobot {
       kMinOutput = min; kMaxOutput = max; 
     }
 
-    // set velocity in PID controller
+    /**
+     * PIDController objects are commanded to a set point using the 
+     * SetReference() method.
+     * 
+     * The first parameter is the value of the set point, whose units vary
+     * depending on the control type set in the second parameter.
+     * 
+     * The second parameter is the control type can be set to one of four 
+     * parameters:
+     *  rev::ControlType::kDutyCycle
+     *  rev::ControlType::kPosition
+     *  rev::ControlType::kVelocity
+     *  rev::ControlType::kVoltage
+     */
     m_pidController.SetReference(rotations, rev::ControlType::kPosition);
     
     frc::SmartDashboard::PutNumber("SetPoint", rotations);
-    frc::SmartDashboard::PutNumber("PV", m_encoder.GetPosition());
+    frc::SmartDashboard::PutNumber("ProcessVariable", m_encoder.GetPosition());
   }
 };
 
