c3672d6d3b8513478999b64d0eeb646505439513
unknown <ryanjharmon1@gmail.com>
2019-01-10 13:21:22 -0600

    Changed name of project

--- C++/Read Encoder Values/src/main/cpp/Robot.cpp
+++ C++/Read Encoder Values/src/main/cpp/Robot.cpp
@@ -8,7 +8,6 @@
 #include <frc/Joystick.h>
 #include <frc/TimedRobot.h>
 #include <frc/smartdashboard/SmartDashboard.h>
-
 #include "rev/CANSparkMax.h"
 
 /**
@@ -17,30 +16,9 @@
  * Position is displayed in revolutions and velocity is displayed in RPM
  */
 class Robot : public frc::TimedRobot {
- public:
-  Robot() { }
-
-  void TeleopPeriodic() override {
-    // set the motor output based on jostick position
-    m_motor.Set(m_stick.GetY());
-
-    // Read the position from the encoder.
-    frc::SmartDashboard::PutNumber("Encoder Position", m_encoder.GetPosition());
-
-    // Read the velocity from the encoder.
-    frc::SmartDashboard::PutNumber("Encoder Rate", m_encoder.GetVelocity());
-  }
-
- private:
-  /**
-   * The motor object is constructed with 2 parameters, a CAN device ID and a 
-   * motor type
-   * 
-   * The CAN ID is configured using the SPARK MAX Client, and defaults to 0
-   * 
-   * The motor type can either be kBrushless or kBrushed
-   */
-  rev::CANSparkMax m_motor{1, rev::CANSparkMax::MotorType::kBrushless};
+  // initialize SPARK MAX
+  static const int deviceID = 1;
+  rev::CANSparkMax m_motor{deviceID, rev::CANSparkMax::MotorType::kBrushless};
 
   /**
    * In order to read encoder values an encoder object is created using the 
@@ -49,6 +27,30 @@ class Robot : public frc::TimedRobot {
   rev::CANEncoder m_encoder = m_motor.GetEncoder();
 
   frc::Joystick m_stick{0};
+
+ public:
+  Robot() { }
+
+  void TeleopPeriodic() override {
+    // set the motor output based on jostick position
+    m_motor.Set(m_stick.GetY());
+
+    /**
+     * Encoder position is read from a CANEncoder object by calling the
+     * GetPosition() method.
+     * 
+     * GetPosition() returns the position of the encoder in units of revolutions
+     */
+    frc::SmartDashboard::PutNumber("Encoder Position", m_encoder.GetPosition());
+
+    /**
+     * Encoder velocity is read from a CANEncoder object by calling the
+     * GetVelocity() method.
+     * 
+     * GetVelocity() returns the velocity of the encoder in units of RPM
+     */
+    frc::SmartDashboard::PutNumber("Encoder Velocity", m_encoder.GetVelocity());
+  }
 };
 
 #ifndef RUNNING_FRC_TESTS
