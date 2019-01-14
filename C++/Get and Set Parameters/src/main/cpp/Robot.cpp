d965f4f2b207b6a4e38834a36aac0c2e1473d095
unknown <ryanjharmon1@gmail.com>
2019-01-14 14:42:24 -0600

    Update Team Number

--- C++/Get and Set Parameters/src/main/cpp/Robot.cpp
+++ C++/Get and Set Parameters/src/main/cpp/Robot.cpp
@@ -6,63 +6,24 @@
 /*----------------------------------------------------------------------------*/
 
 #include <frc/Joystick.h>
+#include <frc/PWMVictorSPX.h>
 #include <frc/TimedRobot.h>
-#include <frc/smartdashboard/smartdashboard.h>
-#include "rev/CANSparkMax.h"
+#include <frc/drive/DifferentialDrive.h>
 
+/**
+ * This is a demo program showing the use of the DifferentialDrive class.
+ * Runs the motors with arcade steering.
+ */
 class Robot : public frc::TimedRobot {
-  /**
-   * deviceID is the CAN ID of the SPARK MAX you are using.
-   * Change to match your setup
-   */
-  static const int deviceID = 1;
-  rev::CANSparkMax m_motor{deviceID, rev::CANSparkMax::MotorType::kBrushless};
-
+  frc::PWMVictorSPX m_leftMotor{0};
+  frc::PWMVictorSPX m_rightMotor{1};
+  frc::DifferentialDrive m_robotDrive{m_leftMotor, m_rightMotor};
   frc::Joystick m_stick{0};
 
  public:
-  void RobotInit() {
-    /**
-     * Parameters can be set by calling the appropriate Set method on the CANSparkMax object
-     * whose properties you want to change
-     * 
-     * Set methods will return one of three CANError values which will let you know if the 
-     * parameter was successfully set:
-     *  CANError::kOk
-     *  CANError::kError
-     *  CANError::kTimeout
-     */
-    if(m_motor.SetIdleMode(rev::CANSparkMax::IdleMode::kCoast) != rev::CANError::kOK) {
-      frc::SmartDashboard::PutString("Idle Mode", "Error");
-    }
-
-    /**
-     * Similarly, parameters will have a Get method which allows you to retrieve their values
-     * from the controller
-     */
-    if(m_motor.GetIdleMode() == rev::CANSparkMax::IdleMode::kCoast) {
-      frc::SmartDashboard::PutString("Idle Mode", "Coast");
-    } else {
-      frc::SmartDashboard::PutString("Idle Mode", "Brake");
-    }
-
-    // Set ramp rate to 0
-    if(m_motor.SetRampRate(0) != rev::CANError::kOK) {
-      frc::SmartDashboard::PutString("Ramp Rate", "Error");
-    }
-
-    // read back ramp rate value
-    frc::SmartDashboard::PutNumber("Ramp Rate", m_motor.GetRampRate());
-  }
-  
   void TeleopPeriodic() {
-    // Set motor output to joystick value
-    m_motor.Set(m_stick.GetY());
-    
-    // periodically read voltage, temperature, and applied output and publish to SmartDashboard
-    frc::SmartDashboard::PutNumber("Voltage", m_motor.GetBusVoltage());
-    frc::SmartDashboard::PutNumber("Temperature", m_motor.GetMotorTemperature());
-    frc::SmartDashboard::PutNumber("Output", m_motor.GetAppliedOutput());
+    // Drive with arcade style
+    m_robotDrive.ArcadeDrive(m_stick.GetY(), m_stick.GetX());
   }
 };
 
