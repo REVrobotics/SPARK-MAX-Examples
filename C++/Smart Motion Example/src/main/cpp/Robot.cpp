8e1f26d980e4e31d26699faa0ae93868cdc66138
unknown <ryanjharmon1@gmail.com>
2019-02-15 11:58:37 -0600

    Updated smart motion parameters and comments

--- C++/Smart Motion Example/src/main/cpp/Robot.cpp
+++ C++/Smart Motion Example/src/main/cpp/Robot.cpp
@@ -14,29 +14,37 @@
  * Before Running:
  * Open shuffleBoard, select File->Load Layout and select the 
  * shuffleboard.json that is in the root directory of this example
- * 
- * Tuning Instructions:
- * 0) Before using this function, you should understand from your specific 
- * mechanism the max acceleration and the max velocity. You can obtain these 
- * either by design constraints, or running in open loop mode and 
- * calculating these values.
- * 
- * 1) Set the 'Smart Motion/Velocity' switch to 'Velocity'
- * 
- * 2) Tune the mechanism as you would a Velocity PID loop until the 
- * mechanism is controlled fairly well. In this mode the 'Set Velocity' slider 
- * controls the Velocity of the mechanism.
- * 
- * 3) Configure the smart motion parameters, and set the 'Mode' switch to 
- * 'Smart Motion'. In this mode, the 'Set Position' text box controls the Position 
- * of the mechansim. For initial tests it is recommended to keep the max velocity 
- * and max acceleration lower than the max value, and slowly increase these to 
- * the desired level.
- * 
- * 4) Test the mechanism by setting the position within the allowable range of 
- * the mechanism, and slowly increase limits until the system is working 
- * as intended.
  */
+
+/**
+ * REV Smart Motion Guide
+ * 
+ * The SPARK MAX includes a new control mode, REV Smart Motion which is used to 
+ * control the position of the motor, and includes a max velocity and max 
+ * acceleration parameter to ensure the motor moves in a smooth and predictable 
+ * way. This is done by generating a motion profile on the fly in SPARK MAX and 
+ * controlling the velocity of the motor to follow this profile.
+ * 
+ * Since REV Smart Motion uses the velocity to track a profile, there are only 
+ * two steps required to configure this mode:
+ *    1) Tune a velocity PID loop for the mechanism
+ *    2) Configure the smart motion parameters
+ * 
+ * Tuning the Velocity PID Loop
+ * 
+ * The most important part of tuning any closed loop control such as the velocity 
+ * PID, is to graph the inputs and outputs to understand exactly what is happening. 
+ * For tuning the Velocity PID loop, at a minimum we recommend graphing:
+ *
+ *    1) The velocity of the mechanism (‘Process variable’)
+ *    2) The commanded velocity value (‘Setpoint’)
+ *    3) The applied output
+ *
+ * This example will use ShuffleBoard to graph the above parameters. Make sure to
+ * load the shuffleboard.json file in the root of this directory to get the full
+ * effect of the GUI layout.
+ */
+
 class Robot : public frc::TimedRobot {
   static const int deviceID = 1;
   rev::CANSparkMax m_motor{deviceID, rev::CANSparkMax::MotorType::kBrushless};
@@ -44,7 +52,7 @@ class Robot : public frc::TimedRobot {
   rev::CANEncoder m_encoder = m_motor.GetEncoder();
 
   // default PID coefficients
-  double kP = 5e-5, kI = 1e-6, kD = 0, kIz = 0, kFF = 0, kMaxOutput = 1, kMinOutput = -1;
+  double kP = 5e-5, kI = 1e-6, kD = 0, kIz = 0, kFF = 0.000156, kMaxOutput = 1, kMinOutput = -1;
 
   // default smart motion coefficients
   double kMaxVel = 2000, kMinVel = 0, kMaxAcc = 1500, kAllErr = 0;
