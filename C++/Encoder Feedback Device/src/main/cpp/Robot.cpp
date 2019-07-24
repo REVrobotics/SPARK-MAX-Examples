a4ec7d148ff3938e0a3494f6b0ee31ace66569d2
Kelly Ostrom <ostrom.ke@gmail.com>
2019-07-24 15:21:58 -0500

    Added encoder feedback device

---
+++ C++/Encoder Feedback Device/src/main/cpp/Robot.cpp
@@ -0,0 +1,122 @@
+/*----------------------------------------------------------------------------*/
+/* Copyright (c) 2017-2018 FIRST. All Rights Reserved.                        */
+/* Open Source Software - may be modified and shared by FRC teams. The code   */
+/* must be accompanied by the FIRST BSD license file in the root directory of */
+/* the project.                                                               */
+/*----------------------------------------------------------------------------*/
+
+#include <frc/TimedRobot.h>
+#include <frc/smartdashboard/smartdashboard.h>
+#include "rev/CANSparkMax.h"
+
+
+
+class Robot : public frc::TimedRobot {
+
+
+  // initialize SPARK MAX with CAN ID
+  static const int deviceID = 1;
+  rev::CANSparkMax m_motor{deviceID, rev::CANSparkMax::MotorType::kBrushless};
+
+  /**
+   * A CANEncoder object is constructed using the GetEncoder() method on an 
+   * existing CANSparkMax object. The assumed encoder type is the hall effect,
+   * or a sensor type and counts per revolution can be passed in to specify
+   * a different kind of sensor. Here, it's a quadrature encoder with 4096 CPR.
+   */
+  rev::CANEncoder m_encoder = m_motor.GetEncoder(rev::SensorType::kEncoder, 4096);
+
+  /**
+   * In order to use PID functionality for a controller, a CANPIDController object
+   * is constructed by calling the GetPIDController() method on an existing
+   * CANSparkMax object. 
+   * 
+   */
+  rev::CANPIDController m_pidController = m_motor.GetPIDController();
+
+  // PID coefficients
+  double kP = 0.1, kI = 1e-4, kD = 1, kIz = 0, kFF = 0, kMaxOutput = 1, kMinOutput = -1;
+
+ public:
+  void RobotInit() {
+    /**
+     * The RestoreFactoryDefaults method can be used to reset the configuration parameters
+     * in the SPARK MAX to their factory default state. If no argument is passed, these
+     * parameters will not persist between power cycles
+     */
+    m_motor.RestoreFactoryDefaults();
+
+    /**
+     * The PID Controller can be configured to use the analog sensor as its feedback
+     * device with the method SetFeedbackDevice() and passing the PID Controller
+     * the CANEncoder object. 
+     */
+   
+    m_pidController.SetFeedbackDevice(m_encoder);
+
+    // set PID coefficients
+    m_pidController.SetP(kP);
+    m_pidController.SetI(kI);
+    m_pidController.SetD(kD);
+    m_pidController.SetIZone(kIz);
+    m_pidController.SetFF(kFF);
+    m_pidController.SetOutputRange(kMinOutput, kMaxOutput);
+
+    // display PID coefficients on SmartDashboard
+    frc::SmartDashboard::PutNumber("P Gain", kP);
+    frc::SmartDashboard::PutNumber("I Gain", kI);
+    frc::SmartDashboard::PutNumber("D Gain", kD);
+    frc::SmartDashboard::PutNumber("I Zone", kIz);
+    frc::SmartDashboard::PutNumber("Feed Forward", kFF);
+    frc::SmartDashboard::PutNumber("Max Output", kMaxOutput);
+    frc::SmartDashboard::PutNumber("Min Output", kMinOutput);
+    frc::SmartDashboard::PutNumber("Set Rotations", 0);
+
+  }
+  void TeleopPeriodic() {
+    // read PID coefficients from SmartDashboard
+    double p = frc::SmartDashboard::GetNumber("P Gain", 0);
+    double i = frc::SmartDashboard::GetNumber("I Gain", 0);
+    double d = frc::SmartDashboard::GetNumber("D Gain", 0);
+    double iz = frc::SmartDashboard::GetNumber("I Zone", 0);
+    double ff = frc::SmartDashboard::GetNumber("Feed Forward", 0);
+    double max = frc::SmartDashboard::GetNumber("Max Output", 0);
+    double min = frc::SmartDashboard::GetNumber("Min Output", 0);
+    double rotations = frc::SmartDashboard::GetNumber("Set Rotations", 0);
+
+    // if PID coefficients on SmartDashboard have changed, write new values to controller
+    if((p != kP)) { m_pidController.SetP(p); kP = p; }
+    if((i != kI)) { m_pidController.SetI(i); kI = i; }
+    if((d != kD)) { m_pidController.SetD(d); kD = d; }
+    if((iz != kIz)) { m_pidController.SetIZone(iz); kIz = iz; }
+    if((ff != kFF)) { m_pidController.SetFF(ff); kFF = ff; }
+    if((max != kMaxOutput) || (min != kMinOutput)) { 
+      m_pidController.SetOutputRange(min, max); 
+      kMinOutput = min; kMaxOutput = max; 
+    }
+
+    /**
+     * PIDController objects are commanded to a set point using the 
+     * SetReference() method.
+     * 
+     * The first parameter is the value of the set point, whose units vary
+     * depending on the control type set in the second parameter.
+     * 
+     * The second parameter is the control type can be set to one of four 
+     * parameters:
+     *  rev::ControlType::kDutyCycle
+     *  rev::ControlType::kPosition
+     *  rev::ControlType::kVelocity
+     *  rev::ControlType::kVoltage
+     */
+    m_pidController.SetReference(rotations, rev::ControlType::kPosition);
+    
+    frc::SmartDashboard::PutNumber("SetPoint", rotations);
+    frc::SmartDashboard::PutNumber("ProcessVariable", m_encoder.GetPosition());
+    
+  }
+};
+
+#ifndef RUNNING_FRC_TESTS
+int main() { return frc::StartRobot<Robot>(); }
+#endif
---
+++ Java/Encoder Feedback Device/src/main/java/frc/robot/Robot.java
@@ -0,0 +1,129 @@
+/*----------------------------------------------------------------------------*/
+/* Copyright (c) 2017-2018 FIRST. All Rights Reserved.                        */
+/* Open Source Software - may be modified and shared by FRC teams. The code   */
+/* must be accompanied by the FIRST BSD license file in the root directory of */
+/* the project.                                                               */
+/*----------------------------------------------------------------------------*/
+
+package frc.robot;
+
+import edu.wpi.first.wpilibj.Joystick;
+import edu.wpi.first.wpilibj.TimedRobot;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
+
+import com.revrobotics.CANEncoder;
+import com.revrobotics.CANDigitalInput;
+import com.revrobotics.CANPIDController;
+import com.revrobotics.CANSparkMax;
+import com.revrobotics.SensorType;
+import com.revrobotics.CANSparkMaxLowLevel.MotorType;
+import com.revrobotics.ControlType;
+
+public class Robot extends TimedRobot {
+  private Joystick m_stick;
+  private static final int deviceID = 1;
+  private CANSparkMax m_motor;
+  private CANPIDController m_pidController;
+  public double kP, kI, kD, kIz, kFF, kMaxOutput, kMinOutput;
+
+  /**
+   * A CANEncoder object is constructed using the GetEncoder() method on an 
+   * existing CANSparkMax object. The assumed encoder type is the hall effect,
+   * or a sensor type and counts per revolution can be passed in to specify
+   * a different kind of sensor. Here, it's a quadrature encoder with 4096 CPR.
+   */
+  private CANEncoder m_encoder;
+
+  @Override
+  public void robotInit() {
+    // initialize SPARK MAX with CAN ID
+    m_motor = new CANSparkMax(deviceID, MotorType.kBrushless);
+    m_encoder = m_motor.getEncoder(SensorType.kEncoder, 4096);
+    
+    m_motor.restoreFactoryDefaults();
+
+    /**
+     * In order to use PID functionality for a controller, a CANPIDController object
+     * is constructed by calling the getPIDController() method on an existing
+     * CANSparkMax object
+     */
+    m_pidController = m_motor.getPIDController();
+  
+    /**
+     * The PID Controller can be configured to use the analog sensor as its feedback
+     * device with the method SetFeedbackDevice() and passing the PID Controller
+     * the CANAnalog object. 
+     */
+    m_pidController.setFeedbackDevice(m_encoder);
+
+    // PID coefficients
+    kP = 0.1; 
+    kI = 1e-4;
+    kD = 1; 
+    kIz = 0; 
+    kFF = 0; 
+    kMaxOutput = 1; 
+    kMinOutput = -1;
+
+    // set PID coefficients
+    m_pidController.setP(kP);
+    m_pidController.setI(kI);
+    m_pidController.setD(kD);
+    m_pidController.setIZone(kIz);
+    m_pidController.setFF(kFF);
+    m_pidController.setOutputRange(kMinOutput, kMaxOutput);
+
+    // display PID coefficients on SmartDashboard
+    SmartDashboard.putNumber("P Gain", kP);
+    SmartDashboard.putNumber("I Gain", kI);
+    SmartDashboard.putNumber("D Gain", kD);
+    SmartDashboard.putNumber("I Zone", kIz);
+    SmartDashboard.putNumber("Feed Forward", kFF);
+    SmartDashboard.putNumber("Max Output", kMaxOutput);
+    SmartDashboard.putNumber("Min Output", kMinOutput);
+    SmartDashboard.putNumber("Set Rotations", 0);
+  }
+
+  @Override
+  public void teleopPeriodic() {
+    // read PID coefficients from SmartDashboard
+    double p = SmartDashboard.getNumber("P Gain", 0);
+    double i = SmartDashboard.getNumber("I Gain", 0);
+    double d = SmartDashboard.getNumber("D Gain", 0);
+    double iz = SmartDashboard.getNumber("I Zone", 0);
+    double ff = SmartDashboard.getNumber("Feed Forward", 0);
+    double max = SmartDashboard.getNumber("Max Output", 0);
+    double min = SmartDashboard.getNumber("Min Output", 0);
+    double rotations = SmartDashboard.getNumber("Set Rotations", 0);
+
+    // if PID coefficients on SmartDashboard have changed, write new values to controller
+    if((p != kP)) { m_pidController.setP(p); kP = p; }
+    if((i != kI)) { m_pidController.setI(i); kI = i; }
+    if((d != kD)) { m_pidController.setD(d); kD = d; }
+    if((iz != kIz)) { m_pidController.setIZone(iz); kIz = iz; }
+    if((ff != kFF)) { m_pidController.setFF(ff); kFF = ff; }
+    if((max != kMaxOutput) || (min != kMinOutput)) { 
+      m_pidController.setOutputRange(min, max); 
+      kMinOutput = min; kMaxOutput = max; 
+    }
+
+    /**
+     * PIDController objects are commanded to a set point using the 
+     * SetReference() method.
+     * 
+     * The first parameter is the value of the set point, whose units vary
+     * depending on the control type set in the second parameter.
+     * 
+     * The second parameter is the control type can be set to one of four 
+     * parameters:
+     *  com.revrobotics.ControlType.kDutyCycle
+     *  com.revrobotics.ControlType.kPosition
+     *  com.revrobotics.ControlType.kVelocity
+     *  com.revrobotics.ControlType.kVoltage
+     */
+    m_pidController.setReference(rotations, ControlType.kPosition);
+    
+    SmartDashboard.putNumber("SetPoint", rotations);
+    SmartDashboard.putNumber("ProcessVariable", m_encoder.getPosition());
+  }
+}
