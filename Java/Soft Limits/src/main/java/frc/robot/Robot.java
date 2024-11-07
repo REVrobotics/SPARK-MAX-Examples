e5ec0858c388651c49525a03e30a6684410f763b
Freddy <freddy@revrobotics.com>
2024-11-06 18:32:44 -0600

    Updated soft limit example code with REVlib 2025 classes and methods

--- Java/Soft Limits/src/main/java/frc/robot/Robot.java
+++ Java/Soft Limits/src/main/java/frc/robot/Robot.java
@@ -11,65 +11,71 @@ import edu.wpi.first.wpilibj.Joystick;
 import edu.wpi.first.wpilibj.TimedRobot;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 
-import com.revrobotics.CANSparkMax;
-import com.revrobotics.CANSparkLowLevel.MotorType;
+import com.revrobotics.spark.SparkLowLevel.MotorType;
+import com.revrobotics.spark.SparkMax;
+import com.revrobotics.spark.SparkBase.PersistMode;
+import com.revrobotics.spark.SparkBase.ResetMode;
+import com.revrobotics.spark.config.SparkMaxConfig;
+import com.revrobotics.spark.config.SparkBaseConfig.IdleMode;
+
 
 public class Robot extends TimedRobot {
-  private Joystick m_stick;
+  private Joystick joystick;
   private static final int deviceID = 1;
-  private CANSparkMax m_motor;
-
+  private static final double forwardLimitDefault = 15.0;
+  private static final double reverseLimitDefault = -15.0;
+  private SparkMax motor;
+  private SparkMaxConfig motorConfig;
   @Override
   public void robotInit() {
-    // initialize SPARK MAX with CAN ID
-    m_motor = new CANSparkMax(deviceID, MotorType.kBrushless);
 
-    /**
-     * The RestoreFactoryDefaults method can be used to reset the configuration parameters
-     * in the SPARK MAX to their factory default state. If no argument is passed, these
-     * parameters will not persist between power cycles
-     */
-    m_motor.restoreFactoryDefaults();
+    // Initialize SPARK MAX with CAN ID
+    motor = new SparkMax(deviceID, MotorType.kBrushless);
 
-    /**
-     * Soft Limits restrict the motion of the motor in a particular direction
-     * at a particular point. Soft limits can be applied in only one direction, 
-     * or both directions at the same time. 
-     * 
-     * If the soft limits are disabled and then re-enabled, they will retain
-     * the last limits that they had for that particular direction.
-     * 
-     * The directions are rev::CANSparkMax::kForward and rev::CANSparkMax::kReverse
-     */
-    m_motor.enableSoftLimit(CANSparkMax.SoftLimitDirection.kForward, true);
-    m_motor.enableSoftLimit(CANSparkMax.SoftLimitDirection.kReverse, true);
+    // Create the SPARK MAX Configurator to hold our desired changes
+    motorConfig = new SparkMaxConfig();
 
-    m_motor.setSoftLimit(CANSparkMax.SoftLimitDirection.kForward, 15);
-    m_motor.setSoftLimit(CANSparkMax.SoftLimitDirection.kReverse, 0);
+    // Chain method calls to set soft limit values
+    motorConfig.softLimit
+      .forwardSoftLimit(forwardLimitDefault)
+      .reverseSoftLimit(reverseLimitDefault)
+      .forwardSoftLimitEnabled(true)
+      .reverseSoftLimitEnabled(true);
+      
+    // Set idle mode to brake
+    motorConfig.idleMode(IdleMode.kBrake);
+    
+    // Restore defaults and before setting our soft limit parameters and saving changes to eeprom 
+    motor.configure(motorConfig, ResetMode.kResetSafeParameters, PersistMode.kPersistParameters);
 
+    // Display current motor parameter values on smart dashboard
     SmartDashboard.putBoolean("Forward Soft Limit Enabled",
-                              m_motor.isSoftLimitEnabled(CANSparkMax.SoftLimitDirection.kForward));
+                              motor.configAccessor.softLimit.getForwardSoftLimitEnabled());
     SmartDashboard.putBoolean("Reverse Soft Limit Enabled",
-                              m_motor.isSoftLimitEnabled(CANSparkMax.SoftLimitDirection.kReverse));                          
+                              motor.configAccessor.softLimit.getReverseSoftLimitEnabled());                          
     SmartDashboard.putNumber("Forward Soft Limit",
-                              m_motor.getSoftLimit(CANSparkMax.SoftLimitDirection.kForward));
+                              motor.configAccessor.softLimit.getForwardSoftLimit());
     SmartDashboard.putNumber("Reverse Soft Limit",
-                              m_motor.getSoftLimit(CANSparkMax.SoftLimitDirection.kReverse));
-
-    m_stick = new Joystick(0);
+                              motor.configAccessor.softLimit.getReverseSoftLimit());
+    SmartDashboard.putNumber("Current Position", motor.getEncoder().getPosition());
+    joystick = new Joystick(0);
   }
 
   @Override
   public void teleopPeriodic() {
-    m_motor.set(m_stick.getY());
+    motor.set(joystick.getY());
 
-    m_motor.enableSoftLimit(CANSparkMax.SoftLimitDirection.kForward, 
-                            SmartDashboard.getBoolean("Forward Soft Limit Enabled", true));
-    m_motor.enableSoftLimit(CANSparkMax.SoftLimitDirection.kReverse, 
-                            SmartDashboard.getBoolean("Reverse Soft Limit Enabled", true));
-    m_motor.setSoftLimit(CANSparkMax.SoftLimitDirection.kForward, 
-                         (float)SmartDashboard.getNumber("Forward Soft Limit", 15));
-    m_motor.setSoftLimit(CANSparkMax.SoftLimitDirection.kReverse,
-                         (float)SmartDashboard.getNumber("Reverse Soft Limit", 0));
+    // Refresh the soft limit configuration based on dashboard values
+    motorConfig.softLimit
+      .forwardSoftLimitEnabled(SmartDashboard.getBoolean("Forward Soft Limit Enabled", true))
+      .reverseSoftLimitEnabled(SmartDashboard.getBoolean("Reverse Soft Limit Enabled", true))
+      .forwardSoftLimit((float)SmartDashboard.getNumber("Forward Soft Limit", forwardLimitDefault))
+      .reverseSoftLimit((float)SmartDashboard.getNumber("Reverse Soft Limit", reverseLimitDefault));
+
+    // Only apply configuration changes to the motor
+    motor.configure(motorConfig, ResetMode.kNoResetSafeParameters, PersistMode.kNoPersistParameters);
+
+    // Display the current position of the motor 
+    SmartDashboard.putNumber("Current Position", motor.getEncoder().getPosition());
   }
 }
